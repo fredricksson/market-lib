e48f4a14f6c0fa668ec7b9e9ebf21e2193b9a26e
fredrikssson <bandefredricksson@gmail.com>
2025-01-27 20:16:44 +0200

    changes

---
+++ src/types/@market__prisma-lib.d.ts
@@ -0,0 +1,8 @@
+// src/types/@market__prisma-lib.d.ts
+declare module '@market/prisma-lib' {
+    import { PrismaClient } from '@prisma/client';
+    
+    const prisma: PrismaClient;
+    export default prisma;
+  }
+  
